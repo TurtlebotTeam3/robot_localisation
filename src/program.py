8da91753a0134a90bba8d03c217e6b76a8db43a5
jan <fleisch.ja@gmail.com>
2020-01-28 18:25:44 +0100

    fixed some bugs

--- src/program.py
+++ src/program.py
@@ -2,10 +2,11 @@ import rospy
 import numpy as np
 import math
 
-from robot_localisation.srv import Localise
+from robot_localisation.srv import Localise, LocaliseResponse
 from geometry_msgs.msg._Twist import Twist
 from geometry_msgs.msg._PoseWithCovarianceStamped import PoseWithCovarianceStamped
 from sensor_msgs.msg._LaserScan import LaserScan
+from std_srvs.srv import Empty, EmptyRequest
 
 class LocaliseRobot:
 
@@ -31,29 +32,41 @@ class LocaliseRobot:
 
         self.localise_robot_service = rospy.Service('localise_robot_service', Localise, self._handle_localise)
 
+        #rospy.spin()
+
+    def run(self):
+
+        while len(self.lidar_data) == 0:
+            self.rate.sleep()
+        self._do_localisation()
 
     def _handle_localise(self, data):
         """
         Handels the request for localisation.
         Starts the localisation behaviour and retruns True when the localisation is finished
         """
+        result = self._do_localisation()
+        return LocaliseResponse(result)
+        
+    def _do_localisation(self): 
         # Initiate global localization, wherein all particles are dispersed randomly through the free space in the map.
         self.global_localisation()
 
         while self.area_ellips > self.epsilon:
             range_front = []
             range_front[:20] = self.lidar_data[-20:]
-		    range_front[20:] = self.lidar_data[:20]
+            range_front[20:] = self.lidar_data[:20]
 
             obstacle_in_front = self._is_obstacle_in_front()
             if obstacle_in_front:
                 # rotate to the right
-                self._move(0, -.5)
-            else
+                self._move(0, -0.5)
+            else:
                 # move straight forward
                 self._move(0.25, 0)
 
         self._move(0, 0)
+        return True
 
     def _is_obstacle_in_front(self):
         """
@@ -63,10 +76,10 @@ class LocaliseRobot:
         range_front[:20] = self.lidar_data[-20:]
         range_front[20:] = self.lidar_data[:20]
         range_front = list(filter(lambda num: num != 0, range_front))
-		min_front = min(range_front)
-		if min_front < 0.25 and min_front != 0.0:
+        min_front = min(range_front)
+        if min_front < 0.4 and min_front != 0.0:
 			return True
-		else:
+        else:
 			return False
 
     def _move(self, linear, angluar):
@@ -75,15 +88,15 @@ class LocaliseRobot:
         """
         vel_msg = Twist()
 		# Linear velocity in the x-axis.
-		vel_msg.linear.x = linear
-		vel_msg.linear.y = 0
-		vel_msg.linear.z = 0
+        vel_msg.linear.x = linear
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
 
 		# Angular velocity in the z-axis.
-		vel_msg.angular.x = 0
-		vel_msg.angular.y = 0
-        vel_msg.angular.y = angluar
-		self.velocity_publisher.publish(vel_msg)
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = angluar
+        self.velocity_publisher.publish(vel_msg)
         self.rate.sleep()
     
     def _handle_scan(self, data):
@@ -97,6 +110,7 @@ class LocaliseRobot:
         Handles data published to the amcl_pose topic
         """
         cov = data.pose.covariance
+        cov = np.reshape(cov,(6,6))
 
         a, b, deg = self._calc_ellipse(cov)
 
@@ -113,8 +127,9 @@ class LocaliseRobot:
         a, b = 2 / np.sqrt(eig_val)
         return a, b, deg
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     try:
         lr = LocaliseRobot()
+        lr.run()
     except rospy.ROSInterruptException:
         pass
